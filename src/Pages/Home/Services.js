4d4cee8c3680f134582a1d3028b95f0ed0002bc1
FarhanBinShafiq <farhanbinshafiq@gmail.com>
2022-12-23 20:03:36 +0600

    Home update

---
+++ src/Pages/Home/Services.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div className='my-28'>
+            <h2>Our Services</h2>
+        </div>
+    );
+};
+
+export default Services;
