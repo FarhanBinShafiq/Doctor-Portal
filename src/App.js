e8351053546c8f8b1f4a110dd5f13d6ad7f3c826
FarhanBinShafiq <farhanbinshafiq@gmail.com>
2022-12-26 22:02:04 +0600

    appointment folder renames in app js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
-import Appoinments from './Pages/Appointmnets/Appoinments';
+import Appoinments from './Pages/Appointments/Appoinments';
 import Contact from './Pages/Contact/Contact';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
