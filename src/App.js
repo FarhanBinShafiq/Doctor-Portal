2d7d067185ed44e0587eafe9e44f240e5eaf6d3c
FarhanBinShafiq <farhanbinshafiq@gmail.com>
2022-12-26 21:42:58 +0600

    appointment page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
-import Appointments from './Pages/Appointmnets/Appointments';
+import Appointments from './Pages/Appointments/Appointments';
 import Contact from './Pages/Contact/Contact';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
