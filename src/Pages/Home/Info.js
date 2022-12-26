c101895c241cea58cb49af397219cc30ba574103
Md.Farhan Bin Shafiq <farhanbinshafiq@gmail.com>
2022-12-26 21:51:59 +0600

    Revert "appointment"

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -6,7 +6,7 @@ import phone from '../../assets/icons/phone.svg';
 
 const Info = () => {
     return (
-        <div className='grid grid-cols-1  lg:grid-cols-3 gap-6'>
+        <div className='grid grid-cols-1 mt-[-100px] lg:grid-cols-3 gap-6'>
             <InfoCard cardTitle="Opening Hours" bgClass='bg-gradient-to-r from-info to-primary' img={clock}/>
             <InfoCard cardTitle="Our Location" bgClass='bg-gradient-to-r from-info to-primary' img={marker}/>
             <InfoCard cardTitle="Our Contact" bgClass='bg-gradient-to-r from-info to-primary' img={phone}/>
